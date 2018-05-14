a9b7460df62284ed57c1c786f59ae0c1f0699995
KernoKristaps <39062321+KernoKristaps@users.noreply.github.com>
2018-05-14 19:42:25 +0300

    1_summit
    for cikli mājas darbs

---
+++ Day_7/Day_7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+            ReizTabula();
+            /*Console.WriteLine("Ievadiet skaitli : ");
+            string ievade = Console.ReadLine();
+            int skaitlis = Convert.ToInt16(ievade);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + "*" + skaitlis + "=" + i * skaitlis);
+            }
+            */
+            Console.ReadLine();
+        }
+        static void ReizTabula()
+        {
+            Console.WriteLine("Ievadiet skaitli : ");
+            string ievade = Console.ReadLine();
+            int skaitlis = Convert.ToInt16(ievade);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + "*" + skaitlis + "=" + i * skaitlis);
+            }
+        }
+
+
+
+    }
+}
