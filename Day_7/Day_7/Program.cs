79afd0cfe4503cec835f272675a0f4fbb284a2be
KernoKristaps <39062321+KernoKristaps@users.noreply.github.com>
2018-05-15 18:58:15 +0300

    4_summit
    Menu izvele ( while cikls )

--- Day_7/Day_7/Program.cs
+++ Day_7/Day_7/Program.cs
@@ -11,32 +11,61 @@ namespace Day_7
         static void Main(string[] args)
         {
 
+            int izvele = 1;
+            while(izvele !=0)
+            {
+                Console.WriteLine("\n" + "1- izvadit reizrekina tabula");
+                Console.WriteLine("2- darbsNr1");
+                Console.WriteLine("3- darbsNr2");
+                Console.WriteLine("4- Faktorialais");
+                Console.WriteLine("0- Iziet\n");
 
+                string input = Console.ReadLine();
+                izvele = Convert.ToInt16(input);
 
+                switch (izvele)
+                {
+                    case 1:
+                        ReizTabula();
+                        break;
+                    case 2:
+                        Darbs();
+                        break;
+                    case 3:
+                        Darbs2();
+                        break;
+                    case 0:
+                        izvele = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Ievade nav pareiza \n");
+                        break;
+                }
+            }
             Darbs2();
 
             for (int i = 10; i > 0; i--)
             {
                 Console.WriteLine(i);
             }
-            Console.ReadLine();
+            
 
             
 
-            ReizTabula();
+            /*ReizTabula();
             Darbs();
             
-            /*Console.WriteLine("Ievadiet skaitli : ");
+            Console.WriteLine("Ievadiet skaitli : ");
             string ievade = Console.ReadLine();
             int skaitlis = Convert.ToInt16(ievade);
 
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine(i + "*" + skaitlis + "=" + i * skaitlis);
-            }
-            */
+            }*/
             
-            Console.ReadLine();
+            
+            
         }
         static void ReizTabula()
         {
@@ -55,11 +84,11 @@ namespace Day_7
             for (int i = 1; i < 4; i++) 
             {
                 n = n + "*";
-                Console.WriteLine(n);
+                Console.WriteLine("\n"+ n);
                 
             }
 
-            Console.ReadLine();
+            
 
         }
         static void Darbs2()
@@ -77,7 +106,7 @@ namespace Day_7
                 Console.WriteLine(OldVer + "+" + i + "=" + Ver);
                 
             }
-            Console.ReadLine();
+            
         }
            
 
